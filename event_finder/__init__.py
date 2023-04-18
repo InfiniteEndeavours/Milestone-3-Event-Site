b6d6f9b48c5801ae3853cae9ab2b0d93bb5c3c13
nyx <ethanpitt01@protonmail.com>
2023-04-18 17:27:03 +0100

    Update __init__.py
    Reformat file

--- event_finder/__init__.py
+++ event_finder/__init__.py
@@ -39,9 +39,10 @@ def fake_data():
 def forbidden(e):
     return render_template("errors/403.html"), 404
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("errors/404.html"), 404
+    return render_template("errors/404.html"), 403
 
 
 def create_flask_app():
