51f14c155736c9a5e818a9f348eed1b35208ae9b
nyx <ethanpitt01@protonmail.com>
2023-04-18 13:15:53 +0100

    Update app.py - Comment out DB table drop and create - Comment out generate fake data

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = create_flask_app()
 
 # Run the app if this file is run directly
 if __name__ == "__main__":
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        fake_data()
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
+    #     fake_data()
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=os.environ.get("DEBUG")
