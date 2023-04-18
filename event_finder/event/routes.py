cbc5754fece018af85b2e58258e2da50b8d336d6
nyx <ethanpitt01@protonmail.com>
2023-04-18 15:15:13 +0100

    Update events page
    events.html
    - Add pagination to events page. 10 events are displayed per page.
    
    routes.py
    - Add pagination query
    
    styles.css
    - Add styles for cards and related elements.

--- event_finder/event/routes.py
+++ event_finder/event/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, session, flash, url_for
+from event_finder import db
+from datetime import datetime
+from event_finder.event.models import Event, User, Profile, Attendance
 
 event = Blueprint("event", __name__, template_folder="../event_finder/templates")
 
@@ -8,3 +11,9 @@ def index():
     return render_template("base.html")
 
 
+@event.route("/events")
+def events():
+    page = request.args.get('page', 1, type=int)
+    current_date = datetime.utcnow().date()
+    event_list = Event.query.filter(Event.date >= current_date).paginate(page=page, per_page=10)
+    return render_template("events.html", events=event_list)
