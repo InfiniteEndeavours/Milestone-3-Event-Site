12d90b091a8313482508c77cb6569aaf36cc2bf3
nyx <ethanpitt01@protonmail.com>
2023-04-18 12:33:53 +0100

    Create registration page
    Create register.html
    
    Update auth/routes.py
    - Add Register route
    
    Update Styles.css
    - Add button styling

--- event_finder/auth/routes.py
+++ event_finder/auth/routes.py
@@ -1,6 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__, template_folder="../event_finder/templates")
+
+
+@auth.route("/register")
+def register():
+    return render_template("register.html")
 
 
 
