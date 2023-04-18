aea5a46f36a18d03e19280508f71b952abe71cda
nyx <ethanpitt01@protonmail.com>
2023-04-18 09:51:48 +0100

    Format event/routes.py - Move index route down a line for formatting

--- event_finder/event/routes.py
+++ event_finder/event/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 event_routes = Blueprint("event", __name__, template_folder="../event_finder/templates")
 
+
 @event_routes.route("/")
 def index():
     return render_template("base.html")
