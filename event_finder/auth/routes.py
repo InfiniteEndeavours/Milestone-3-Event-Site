9767cf827e5edaa3ee95a2e141f3489fc0962c83
nyx <ethanpitt01@protonmail.com>
2023-04-23 15:09:11 +0100

    Update auth/routes.py Run file through pep8ci Linter - Fixed lines that were too long

--- event_finder/auth/routes.py
+++ event_finder/auth/routes.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, session, flash, url_for
+from flask import (Blueprint, render_template,
+                   request, redirect, session, flash, url_for)
 from event_finder import db
 from event_finder.event.models import User, Profile
 from werkzeug.security import generate_password_hash, check_password_hash
 
-auth = Blueprint("auth", __name__, template_folder="../event_finder/templates")
+auth = Blueprint("auth", __name__,
+                 template_folder="../event_finder/templates")
 
 
 # Helper Functions
@@ -19,7 +21,8 @@ def validate_password(password, password_confirm):
     Returns:
         - "valid" if the passwords match and also meet the criteria.
         - "no_match" if the passwords do not match.
-        - "not_complex_enough" if the password does not feature special characters.
+        - "not_complex_enough" if the password does not
+         feature special characters.
     """
     # Check if passwords match and are valid
     if password != password_confirm:
@@ -27,7 +30,8 @@ def validate_password(password, password_confirm):
     if not (any(x.isupper() for x in password) and
             any(x.islower() for x in password) and
             any(x.isdigit() for x in password) and
-            any(x in list("!@#$%^&*()_+{}|:'<>?-=[]\\;,./`~]*") for x in password)):
+            any(x in list("!@#$%^&*()_+{}|:'<>?-=[]\\;,./`~]*")
+                for x in password)):
         return "not_complex_enough"
     return "valid"
 
@@ -35,18 +39,25 @@ def validate_password(password, password_confirm):
 @auth.route("/register", methods=["GET", "POST"])
 def register():
     """
-    Route to handle requests to register a new user. Accepts both GET and POST requests.
-    
-    When POST request is recieved, it will process user input from the form and then validate the input.
-    When the input is determined to be valid, the user is created, assigned a session
+    Route to handle requests to register a new user.
+     Accepts both GET and POST requests.
+
+    When POST request is recieved, it will process user input
+     from the form and then validate the input.
+    When the input is determined to be valid, the user is
+     created, assigned a session
      cookie and returned to the profile page.
-     If the request is a GET request (page load), it will render the registration page.
+     If the request is a GET request (page load),
+      it will render the registration page.
 
      Returns:
-        If the request is a GET request, returns the rendered version of "register.html" template.
-        If the request is a POST request and the user input is valid, then login,
+        If the request is a GET request, returns the rendered
+         version of "register.html" template.
+        If the request is a POST request and the user input
+         is valid, then login,
          assign session cookie and redirect to profile page.
-        If the request is a POST request and the user input is invalid, then redirect the user
+        If the request is a POST request and the user input
+         is invalid, then redirect the user
         to registration page and flash an error
     """
     if request.method == "POST":
@@ -64,7 +75,8 @@ def register():
             return redirect(url_for("auth.register"))
 
         # Check if passwords match and are valid
-        valid_password = validate_password(form_password, form_password_confirm)
+        valid_password = validate_password(
+            form_password, form_password_confirm)
         # Declare a variable to store the password hash
         password_hash = ""
 
@@ -73,12 +85,16 @@ def register():
             flash("The passwords you entered do not match.")
             return redirect(url_for('auth.register'))
         elif valid_password == "not_complex_enough":
-            flash("The password you have entered is not complex enough."
-                  "It requires at least one uppercase, one lowercase, one digit and a special character.")
+            flash(
+                "The password you have entered is not complex enough."
+                "It requires at least one uppercase, one lowercase,"
+                " one digit and a special character.")
             return redirect(url_for('auth.register'))
         elif valid_password == "valid":
-            # Generates a password hash using pbkdf2:sha256 algorithm, iterates 150000 times, salts with a length of 20
-            password_hash = generate_password_hash(form_password, method='pbkdf2:sha256:150000', salt_length=20)
+            # Generates a password hash using pbkdf2:sha256 algorithm, iterates
+            # 150000 times, salts with a length of 20
+            password_hash = generate_password_hash(
+                form_password, method='pbkdf2:sha256:150000', salt_length=20)
 
         # Create a new user
         new_user = User(username=username, password=password_hash)
@@ -86,7 +102,10 @@ def register():
         db.session.commit()
 
         # Create a new profile
-        new_profile = Profile(first_name=first_name, last_name=last_name, user_uuid=new_user.uuid)
+        new_profile = Profile(
+            first_name=first_name,
+            last_name=last_name,
+            user_uuid=new_user.uuid)
         db.session.add(new_profile)
         db.session.commit()
 
@@ -105,11 +124,13 @@ def login():
     """
         Logs in a user.
 
-        If a GET request is received, render the login form. If a POST request is received, validate user credentials
+        If a GET request is received, render the login form.
+         If a POST request is received, validate user credentials
         and log in the user if they are valid.
 
         Returns:
-            A rendered HTML template if a GET request is received, or a direct response if a POST request is received.
+            A rendered HTML template if a GET request is received,
+             or a direct response if a POST request is received.
     """
     if request.method == "POST":
         username: str = request.form.get('username').lower()
@@ -133,8 +154,10 @@ def login():
                 return redirect(url_for("auth.login"))
         else:
             # If username doesn't exist return to login page
-            flash("Incorrect Username/Password."
-                  "If you have forgotten your details, please register a new account.")
+            flash(
+                "Incorrect Username/Password."
+                "If you have forgotten your details,"
+                " please register a new account.")
             return render_template("auth/login.html")
 
     return render_template("auth/login.html")
