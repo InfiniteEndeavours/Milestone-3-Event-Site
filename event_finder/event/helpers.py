db48ceb2dc592adb3d8b2f649e640edd003bfa2a
nyx <ethanpitt01@protonmail.com>
2023-04-25 19:24:12 +0100

    Update event/helpers.py
    - Edit docstring with example

--- event_finder/event/helpers.py
+++ event_finder/event/helpers.py
@@ -5,13 +5,14 @@ def db_find_first(model, **kwargs):
     """
     Queries a database model and returns the first result found.
 
-    Args:
+    :param:
     model: SQLAlchemy model Object representing a database table.
     **kwargs: One or more keyword arguments representing the filter criteria.
     The keywords should match the column name and
      the value should be the desired value.
+     example: db_find_first(User, username='admin')
 
-    Returns:
+    :return
     The first row from the query that matches the filter criteria,
      or None if no matching row is found.
     """
