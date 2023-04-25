9fec2cad9ea75402187c925c95fbfff4c9b90290
nyx <ethanpitt01@protonmail.com>
2023-04-25 19:37:54 +0100

    Update app.py
    - Remove DB import
    - Remove fake_data function import
    - Remove commented code as it was only used during testing

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from event_finder import create_flask_app, fake_data, db
+from event_finder import create_flask_app
 import os
 
 app = create_flask_app()
 
 # Run the app if this file is run directly
 if __name__ == "__main__":
-    # with app.app_context():
-    #     db.drop_all()
-    #     db.create_all()
-    #     fake_data()
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=os.environ.get("DEBUG")
