e286245401f12b481379ab9307d2054ecf1f5f32
nyx <ethanpitt01@protonmail.com>
2023-04-25 19:18:32 +0100

    Update admin/routes.py
    - Add docstrings to routes

--- event_finder/admin/routes.py
+++ event_finder/admin/routes.py
@@ -10,6 +10,14 @@ admin = Blueprint("admin", __name__,
 
 @admin.route('/admin')
 def admin_page():
+    """
+    admin_page function
+    If the user who loads the page is an admin, they are shown the admin page,
+    else they are diverted to a 403 and told they are not authorised.
+    :return:
+        admin.html if authorised
+        403.html if unauthorised
+    """
     uuid = session.get("user_uuid")
     user = db_find_first(User, uuid=uuid)
     if user.admin:
@@ -23,6 +31,16 @@ def admin_page():
 
 @admin.route('/admin/delete-event/<int:event_id>', methods=["POST"])
 def delete_event(event_id):
+    """
+    Admin delete event
+    If the admin chooses to delete an event, the event ID is passed in
+    to the db_find_first helper function. This queries the
+    event table to find the matching event. Then all
+    entries relating to the event in the attendance table are deleted.
+    This is a prerequisite to deleting the event from the Event table.
+    :return:
+        admin.html with a flash confirming deletion
+    """
     event = db_find_first(Event, id=event_id)
     Attendance.query.filter_by(event_id=event.id).delete()
     db.session.delete(event)
