f5db9651f6fb3664c5341e95596eef102465c592
nyx <ethanpitt01@protonmail.com>
2023-04-25 19:26:45 +0100

    Update event/models.py
    - Add comments to Models

--- event_finder/event/models.py
+++ event_finder/event/models.py
@@ -3,28 +3,34 @@ from event_finder import db
 from uuid import uuid4
 
 
+# User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # Generate a unique id for each user
+    # Generate a unique id for each user using uuid4 template
     uuid = db.Column(db.String(36), unique=True,
                      nullable=False, default=str(uuid4()))
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255))
     admin = db.Column(db.Boolean, default=False)
+    # One-to-One relationship with Profile
     profile = db.relationship('Profile',
                               backref='user', uselist=False)
+    # One-to-Many relationship with Event
     events_created = db.relationship('Event',
                                      backref='creator', lazy='dynamic')
 
 
+# Profile Model
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+    # Foreign key to User.uuid
     user_uuid = db.Column(db.String(36),
                           db.ForeignKey("user.uuid"), unique=True)
 
 
+# Event Model
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -36,10 +42,12 @@ class Event(db.Model):
     end_time = db.Column(db.Time, nullable=False,
                          default=time(hour=17, minute=0))
     location = db.Column(db.String(255), nullable=False)
+    # Foreign key to User.uuid
     creator_id = db.Column(db.String(36),
                            db.ForeignKey("user.uuid"), nullable=False)
 
 
+# Attendance Model
 class Attendance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
