91c2532ec76700e2e5f750120d07927df835be69
nyx <ethanpitt01@protonmail.com>
2023-04-25 19:23:08 +0100

    Update event/data/fake_data_Generator.py
    - Add comments to script
    - Remove unused import

--- event_finder/event/data/fake_data_generator.py
+++ event_finder/event/data/fake_data_generator.py
@@ -1,14 +1,22 @@
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash
 from faker import Faker
 from event_finder import db
 from event_finder.event.models import User, Profile, Event, Attendance
 from uuid import uuid4
 
+# Create a Faker instance
 fake = Faker()
 
 
 def create_users(num_users):
+    """
+    Create a number of fake users
+    :param num_users:
+    :return:
+        Doesn't return anything but will add Users and their profile
+        to the database
+    """
     for i in range(num_users):
         # Create a new profile for the user
         profile = Profile(first_name=fake.first_name(),
@@ -24,16 +32,26 @@ def create_users(num_users):
 
 
 def create_events(num_events):
+    """
+    Create a number of fake events
+    :param num_events:
+    :return:
+        Doesn't return anything but will add Events to the database
+    """
     for i in range(num_events):
         title = fake.sentence()
         description = fake.text()
+        # Create a date between today and 30 days from now
         date = fake.date_between(start_date='today', end_date='+30d')
+        # Create a start time in correct format
         start_time = datetime.strptime(fake.time(
             pattern='%H:%M:%S'), '%H:%M:%S').time()
+        # Create an end time 2 hours after the start time
         end_time = (datetime.combine(date, start_time)
                     + timedelta(hours=2)).time()
         location = fake.address()
         creator = User.query.order_by(db.func.random()).first()
+        # Create the event
         event = Event(title=title, description=description,
                       date=date, start_time=start_time, end_time=end_time,
                       location=location, creator_id=creator.uuid)
@@ -42,6 +60,12 @@ def create_events(num_events):
 
 
 def create_attendances(num_attendances):
+    """
+    Create a number of fake attendances
+    :param num_attendances:
+    :return:
+        Doesn't return anything but will add Attendances to the database
+    """
     for i in range(num_attendances):
         user = User.query.order_by(db.func.random()).first()
         event = Event.query.order_by(db.func.random()).first()
